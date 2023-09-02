0bd0091e5f6f19e851bce6065e8a06bb113c4cca
Nguyễn Thu Hiền <hiennt2@bap.jp>
2023-09-02 20:29:57 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello Edit Hello <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
